473ab590f23d2b88e0f80d1a1906f9def9f6dfa7
Guillaume <guillaume@livingdata.co>
2024-09-18 17:53:16 +0200

    Update mergeResults to check better result between indexes

--- api/operations/batch.js
+++ api/operations/batch.js
@@ -1,9 +1,36 @@
+import {maxBy} from 'lodash-es'
+
 export default async function batch(payload, options = {}) {
-  const {indexes, signal} = options
-  const results = await indexes.dispatchRequest({...payload, indexes: ['address']}, 'batch', {signal})
-  return mergeResults(results, payload)
+  const {indexes, signal, index} = options
+  const results = await indexes.dispatchRequest({...payload, indexes: index}, 'batch', {signal})
+
+  return mergeResults(results)
 }
 
-export function mergeResults(indexesResults) {
-  return indexesResults.address
+const mergeResults = indexesResults => {
+  const results = []
+
+  const keys = Object.keys(indexesResults)
+
+  for (let i = 0; i < indexesResults[keys[0]].length; i++) {
+    const items = keys.map(key => indexesResults[key][i])
+    const validResults = items.filter(item => item.status === 'ok')
+    const bestResult = maxBy(validResults, item => item.result.score)
+
+    if (bestResult) {
+      results.push({
+        status: bestResult.status,
+        result: bestResult.result,
+        index: keys.find(key => indexesResults[key][i] === bestResult)
+      })
+    } else {
+      results.push({
+        status: 'not-found',
+        result: {},
+        index: keys.find(key => indexesResults[key][i].status === 'not-found')
+      })
+    }
+  }
+
+  return results
 }
