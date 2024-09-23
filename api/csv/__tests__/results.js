4b2abc949f713d7ac8a8b42651008ea313348cc1
Guillaume <guillaume@livingdata.co>
2024-09-23 16:51:06 +0200

    Update tests

--- api/csv/__tests__/results.js
+++ api/csv/__tests__/results.js
@@ -2,7 +2,7 @@
 import test from 'ava'
 import {createEmptyResultItem, convertResultItem, expandItemWithResult} from '../results.js'
 
-test('createEmptyResultItem - search operation', t => {
+test('createEmptyResultItem - search operation - address index', t => {
   const indexes = ['address']
   const operation = 'search'
   const expected = {
@@ -24,18 +24,43 @@ test('createEmptyResultItem - search operation', t => {
     result_oldcitycode: '',
     result_oldcity: '',
     result_district: '',
+    result_importance: '',
+    result_x: '',
+    result_y: '',
     result_status: ''
   }
   t.deepEqual(createEmptyResultItem(indexes, operation), expected)
 })
 
-test('createEmptyResultItem - reverse operation', t => {
+test('createEmptyResultItem - search operation - poi index', t => {
+  const indexes = ['poi']
+  const operation = 'search'
+  const expected = {
+    result_index: '',
+    result_name: '',
+    result_toponym: '',
+    result_category: '',
+    result_postcode: '',
+    result_citycode: '',
+    result_city: '',
+    result_classification: '',
+    result_territory: '',
+    result_score: '',
+    result_score_next: '',
+    latitude: '',
+    longitude: '',
+    result_status: ''
+  }
+  t.deepEqual(createEmptyResultItem(indexes, operation), expected)
+})
+
+test('createEmptyResultItem - reverse operation - address index', t => {
   const indexes = ['address']
   const operation = 'reverse'
   const expected = {
     result_index: '',
-    result_latitude: '',
-    result_longitude: '',
+    latitude: '',
+    longitude: '',
     result_distance: '',
     result_label: '',
     result_type: '',
@@ -50,12 +75,106 @@ test('createEmptyResultItem - reverse operation', t => {
     result_oldcitycode: '',
     result_oldcity: '',
     result_district: '',
+    result_importance: '',
+    result_x: '',
+    result_y: '',
     result_status: ''
   }
   t.deepEqual(createEmptyResultItem(indexes, operation), expected)
 })
 
-test('convertResultItem', t => {
+test('createEmptyResultItem - reverse operation - poi index', t => {
+  const indexes = ['poi']
+  const operation = 'reverse'
+  const expected = {
+    result_index: '',
+    result_name: '',
+    result_toponym: '',
+    result_category: '',
+    result_postcode: '',
+    result_citycode: '',
+    result_city: '',
+    result_classification: '',
+    result_territory: '',
+    result_score: '',
+    result_score_next: '',
+    latitude: '',
+    longitude: '',
+    result_status: ''
+  }
+  t.deepEqual(createEmptyResultItem(indexes, operation), expected)
+})
+
+test('createEmptyResultItem - search operation - address & poi indexes', t => {
+  const indexes = ['address', 'poi']
+  const operation = 'search'
+  const expected = {
+    result_index: '',
+    latitude: '',
+    longitude: '',
+    result_category: '',
+    result_classification: '',
+    result_score: '',
+    result_score_next: '',
+    result_label: '',
+    result_type: '',
+    result_id: '',
+    result_housenumber: '',
+    result_name: '',
+    result_street: '',
+    result_postcode: '',
+    result_city: '',
+    result_context: '',
+    result_citycode: '',
+    result_oldcitycode: '',
+    result_oldcity: '',
+    result_district: '',
+    result_toponym: '',
+    result_territory: '',
+    result_importance: '',
+    result_x: '',
+    result_y: '',
+    result_status: ''
+  }
+  t.deepEqual(createEmptyResultItem(indexes, operation), expected)
+})
+
+test('createEmptyResultItem - reverse operation - address & poi indexes', t => {
+  const indexes = ['address', 'poi']
+  const operation = 'reverse'
+  const expected = {
+    result_index: '',
+    latitude: '',
+    longitude: '',
+    result_category: '',
+    result_classification: '',
+    result_score: '',
+    result_score_next: '',
+    result_distance: '',
+    result_label: '',
+    result_type: '',
+    result_id: '',
+    result_housenumber: '',
+    result_name: '',
+    result_street: '',
+    result_postcode: '',
+    result_city: '',
+    result_context: '',
+    result_citycode: '',
+    result_oldcitycode: '',
+    result_oldcity: '',
+    result_district: '',
+    result_toponym: '',
+    result_territory: '',
+    result_importance: '',
+    result_x: '',
+    result_y: '',
+    result_status: ''
+  }
+  t.deepEqual(createEmptyResultItem(indexes, operation), expected)
+})
+
+test('convertResultItem - address index', t => {
   const resultItem = {
     status: 'OK',
     index: 'address',
@@ -81,6 +200,32 @@ test('convertResultItem', t => {
   t.deepEqual(convertResultItem(resultItem, emptyResultItem), expected)
 })
 
+test('convertResultItem - poi index', t => {
+  const resultItem = {
+    status: 'OK',
+    index: 'poi',
+    result: {
+      lat: '40.7128',
+      lon: '-74.0060',
+      label: 'New York',
+      type: 'city',
+      id: '123'
+    }
+  }
+  const emptyResultItem = createEmptyResultItem(['poi'], 'search')
+  const expected = {
+    ...emptyResultItem,
+    result_index: 'poi',
+    result_label: 'New York',
+    result_type: 'city',
+    result_id: '123',
+    result_status: 'OK',
+    latitude: '40.7128',
+    longitude: '-74.0060'
+  }
+  t.deepEqual(convertResultItem(resultItem, emptyResultItem), expected)
+})
+
 test('expandItemWithResult - with resultColumns', t => {
   const item = {someInputKey: 'someInputValue'}
   const resultItem = {
@@ -96,6 +241,31 @@ test('expandItemWithResult - with resultColumns', t => {
     }
   }
   const emptyResultItem = createEmptyResultItem(['address'], 'search')
+  const resultColumns = ['latitude', 'longitude', 'result_label']
+  const expected = {
+    latitude: '40.7128',
+    longitude: '-74.0060',
+    result_label: 'New York',
+    someInputKey: 'someInputValue'
+  }
+  t.deepEqual(expandItemWithResult(item, resultItem, emptyResultItem, resultColumns), expected)
+})
+
+test('expandItemWithResult - with resultColumns - index poi', t => {
+  const item = {someInputKey: 'someInputValue'}
+  const resultItem = {
+    status: 'OK',
+    index: 'poi',
+    result: {
+      lat: '40.7128',
+      lon: '-74.0060',
+      label: 'New York',
+      type: 'city',
+      id: '123',
+      someKey: 'someValue'
+    }
+  }
+  const emptyResultItem = createEmptyResultItem(['poi'], 'search')
   const resultColumns = ['latitude', 'longitude', 'result_label']
   const expected = {
     latitude: '40.7128',
