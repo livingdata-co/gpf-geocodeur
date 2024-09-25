2325ff9b67e3dcbcc8fb2ae8a6d1f6b27ba03c74
Guillaume <guillaume@livingdata.co>
2024-09-25 16:41:29 +0200

    Adds tests for mergeResults

--- api/operations/__tests__/batch.js
+++ api/operations/__tests__/batch.js
@@ -5,31 +5,47 @@ import {mergeResults} from '../batch.js'
 test('mergeResults should return the best result', t => {
   const indexesResults = {
     index1: [
-      {status: 'ok', result: {score: 1}}
+      {status: 'ok', result: {score: 1}},
+      {status: 'ok', result: {score: 3}},
+      {status: 'ok', result: {score: 2}},
+      {status: 'not-found', result: {}}
     ],
     index2: [
-      {status: 'ok', result: {score: 3}}
+      {status: 'ok', result: {score: 3}},
+      {status: 'ok', result: {score: 2}},
+      {status: 'ok', result: {score: 1.5}},
+      {status: 'ok', result: {score: 0.5}}
     ]
   }
 
   const result = mergeResults(indexesResults)
 
-  t.deepEqual(result, [{status: 'ok', result: {score: 3}, index: 'index2'}])
+  t.deepEqual(result, [
+    {status: 'ok', result: {score: 3}, index: 'index2'},
+    {status: 'ok', result: {score: 3}, index: 'index1'},
+    {status: 'ok', result: {score: 2}, index: 'index1'},
+    {status: 'ok', result: {score: 0.5}, index: 'index2'}
+  ])
 })
 
 test('mergeResults should return the error result if any and no successful result', t => {
   const indexesResults = {
     index1: [
-      {status: 'not-found'}
+      {status: 'not-found'},
+      {status: 'ok', result: {score: 3}}
     ],
     index2: [
+      {status: 'error', result: {message: 'error'}},
       {status: 'error', result: {message: 'error'}}
     ]
   }
 
   const result = mergeResults(indexesResults)
 
-  t.deepEqual(result, [{status: 'error', result: {message: 'error'}, index: 'index2'}])
+  t.deepEqual(result, [
+    {status: 'error', result: {message: 'error'}},
+    {status: 'ok', result: {score: 3}, index: 'index1'}
+  ])
 })
 
 test('mergeResults should return not-found if no successful result and no error result', t => {
