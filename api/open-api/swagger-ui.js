59a12c0b68cd947623a615267c90cf69ab1ad816
Jérôme Desboeufs <jerome@livingdata.co>
2024-11-26 17:07:15 +0100

    fix(doc): IGNGPF-4482 Update SwaggerUI to version 5

--- api/open-api/swagger-ui.js
+++ api/open-api/swagger-ui.js
@@ -5,11 +5,11 @@ export function computeHtmlPage({pageTitle, openApiDefinitionUrl}) {
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>${pageTitle}</title>
-  <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui.css" />
+  <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@^5.18.2/swagger-ui.css" />
 </head>
 <body>
 <div id="swagger-ui"></div>
-<script src="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui-bundle.js" crossorigin></script>
+<script src="https://unpkg.com/swagger-ui-dist@^5.18.2/swagger-ui-bundle.js" crossorigin></script>
 <script>
   window.onload = () => {
     window.ui = SwaggerUIBundle({
