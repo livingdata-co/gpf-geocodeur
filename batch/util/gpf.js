f34d16cb7a85513dad65cc2dd7dde592165caaf5
Valentin VINCENT <valentin.vincent@worldline.com>
2024-12-04 20:54:44 +0000

    fix:IGNGPF-4481 Add proxy config

--- batch/util/gpf.js
+++ batch/util/gpf.js
@@ -1,12 +1,34 @@
 import got from 'got'
+import {HttpProxyAgent, HttpsProxyAgent} from 'hpagent'
 
-import {GPF_API_URL} from '../../lib/config.js'
+import {GPF_API_URL, HTTP_PROXY, HTTPS_PROXY} from '../../lib/config.js'
+
+function getAgent() {
+  const agent = {}
+
+  if (HTTP_PROXY) {
+    agent.http = new HttpProxyAgent({
+      proxy: HTTP_PROXY
+    })
+  }
+
+  if (HTTPS_PROXY) {
+    agent.https = new HttpsProxyAgent({
+      proxy: HTTPS_PROXY
+    })
+  }
+
+  return agent
+}
+
+const agent = getAgent()
 
 export async function getUserInfo(token) {
   const user = await got(`${GPF_API_URL}/users/me`, {
     headers: {
       Authorization: `Bearer ${token}`
-    }
+    },
+    agent
   }).json()
 
   return user
